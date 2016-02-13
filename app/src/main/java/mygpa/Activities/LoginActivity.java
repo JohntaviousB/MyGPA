b7feb952062d57653f8d1e43d16fbdff3cf7aeaa
Johntavious Barkley <Johntavious Barkley>
2016-02-13 03:00:59 -0500

    Created some sexy xml files for Login and Home

--- app/src/main/java/mygpa/Activities/LoginActivity.java
+++ app/src/main/java/mygpa/Activities/LoginActivity.java
@@ -1,18 +1,15 @@
 package mygpa.Activities;
 
-import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
+import android.os.Bundle;
 
 import mygpa.Objects.R;
 
-public class MainActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
     }
-
 }
